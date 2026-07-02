33ea061830a7dc68fc3f1e32ca37140e2e3b71fc
SrbuhiHambardzumyan <srbuhihambardzumyan1993@gmail.com>
2026-07-02 12:31:39 +0400

    Lesson_21_workshop.py

--- lesson_21_workshop.py
+++ lesson_21_workshop.py
@@ -4,8 +4,6 @@ print("Srbuhi Hambardzumyan")
 print()  # Skip one line
 
 print("I am learning Python programming.")
-# You can write any background information about yourself
-
 num1 = 15
 num2 = 25
 sum_result = num1 + num2
